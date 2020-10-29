8d275e79178a5d74edfce46c58d9ba90bc0ddd74
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2020-10-29 22:26:04 +0200

    8777

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import './styles.css';
-const Handlebars = require("handlebars");
-const template = Handlebars.compile("Name: {{name}}");
-console.log(template({ name: "Nils" }));
 
+import name from  './export.js';
+
+// const Theme = {
+//     LIGHT: 'light-theme',
+//     DARK: 'dark-theme',
+//   };
+
+const body = document.querySelector('body');
+const onClickBlackTheme = document.querySelector('.theme-switch__control');
+
+onClickBlackTheme.addEventListener('change', changeFn)
+
+if (localStorage.getItem('body') == 'dark-theme') {
+body.classList.add('dark-theme');
+onClickBlackTheme.classList.add('checked')
+}
+function changeFn(p) {
+    body.classList.toggle('dark-theme');
+if (localStorage.getItem('body') == 'dark-theme') {
+localStorage.removeItem("body", "dark-theme");
+} else {
+localStorage.setItem("body", "dark-theme");
+}
+}
